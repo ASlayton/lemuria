b3a21f3850b0044f53e671531ea498146bd882cb
Amanda Slayton <alslayton86@gmail.com>
2018-07-16 18:35:44 -0500

    GameScreen and Home component made

---
+++ src/components/GameScreen/GameScreen.js
@@ -0,0 +1,12 @@
+import 'GameScreen.css';
+import React from 'react';
+
+class GameScreen extends React.Component {
+  render () {
+    return (
+      <h1>Main Screen</h1>
+    );
+  }
+};
+
+export default GameScreen;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import 'Home.css';
+import React from 'react';
+
+class Home extends React.Component {
+  render () {
+    return (
+      <h1>Welcome to Lemuria</h1>
+    );
+  }
+};
+
+export default Home;
