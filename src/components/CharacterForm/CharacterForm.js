1f345146a6b2073f684fe98c9275eb19c03eaa3a
Amanda Slayton <alslayton86@gmail.com>
2018-07-30 11:40:39 -0500

    roll the die button

--- src/components/CharacterForm/CharacterForm.js
+++ src/components/CharacterForm/CharacterForm.js
@@ -3,21 +3,24 @@ import PropTypes from 'prop-types';
 import dieroll from '../../helpers/dieroll';
 import './CharacterForm.css';
 
-const rollHealth = dieroll(10, 25);
-const rollPsyche = dieroll(10, 25);
+const fortitude = dieroll(1,6) + dieroll(1,6) + dieroll(1,6);
+const strength = dieroll(1,6) + dieroll(1,6) + dieroll(1,6);
+const resilience = dieroll(1,6) + dieroll(1,6) + dieroll(1,6);
+const constitution = dieroll(1,6) + dieroll(1,6) + dieroll(1,6);
 
 const defaultCharacter = {
   profilePic: 'https://upload.wikimedia.org/wikipedia/commons/1/1e/Default-avatar.jpg',
   name: '',
-  totalHealth: rollHealth,
-  currentHealth: rollHealth,
-  totalPsyche: rollPsyche,
-  currentPsyche: rollPsyche,
-  defense: dieroll(1, 20),
-  strength: dieroll(1, 20),
-  fortitude: dieroll(1, 20),
-  resilience: dieroll(1, 20),
-  constitution: dieroll(1, 20),
+  totalHealth: 25,
+  currentHealth: 25,
+  totalPsyche: 25,
+  currentPsyche: 25,
+  defense: 13,
+  attack: 0,
+  strength: strength,
+  fortitude: fortitude,
+  resilience: resilience,
+  constitution: constitution,
   exp: 0,
   level: 0,
   lifeSigns: true,
@@ -34,20 +37,10 @@ class CharacterForm extends React.Component {
     fortCount: 0,
     resCount: 0,
     strCount: 0,
-  };
-
-  evalStats = (val) => {
-    if (val < 4) {
-      return (-2);
-    } else if (val >= 4 && val < 10) {
-      return (-1);
-    } else if (val >= 10 && val < 13) {
-      return (0);
-    } else if (val >= 13 && val < 18) {
-      return (1);
-    } else {
-      return (2);
-    }
+    conRolled: false,
+    resRolled: false,
+    strRolled: false,
+    fontRolled: false,
   };
 
   addConPoint = () => {
@@ -60,10 +53,6 @@ class CharacterForm extends React.Component {
       const tempConCount = (this.state.conCount * 1) + 1;
       this.setState({conCount: tempConCount});
     }
-    const modifier = this.evalStats(this.state.newCharacter.constitution);
-    const modCharacter = {...this.state.newCharacter};
-    modCharacter.totalHeath = (this.state.newCharacter.totalHealth * 1) + modifier;
-    this.setState({newCharacter: modCharacter});
   };
   subtractConPoint = () => {
     if (this.state.conCount > -3) {
@@ -78,7 +67,6 @@ class CharacterForm extends React.Component {
   };
 
   addFortPoint = () => {
-    console.log(this.state.fortCount);
     if (this.state.pointsPot > 0 && this.state.fortCount < 3) {
       const tempCharacter = {...this.state.newCharacter};
       tempCharacter.fortitude = (this.state.newCharacter.fortitude * 1) + 1;
@@ -113,7 +101,7 @@ class CharacterForm extends React.Component {
     }
   };
   subtractStrPoint = () => {
-    if (this.state.conCount > -3) {
+    if (this.state.strCount > -3) {
       const tempCharacter = {...this.state.newCharacter};
       tempCharacter.strength = (this.state.newCharacter.strength * 1) - 1;
       this.setState({newCharacter: tempCharacter});
@@ -176,6 +164,128 @@ class CharacterForm extends React.Component {
     };
   }
 
+  conButtons = () => {
+    if (this.state.conRolled) {
+      return (
+        <div>
+          <div>
+            <h3>Health: {this.state.newCharacter.totalHealth}</h3>
+          </div>
+          <div>
+            <h3>Constitution: {this.state.newCharacter.constitution}</h3>
+            <button type="button" onClick={this.subtractConPoint.bind()} className="btn btn-default"><span className="glyphicon glyphicon-minus"></span></button>
+            <button type="button" onClick={this.addConPoint.bind()} className="btn btn-default"><span className="glyphicon glyphicon-plus"></span></button>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <h3>Roll Constitution:</h3>
+          <button onClick={this.rollTheDieCon.bind()}><span><img src="https://www.spreadshirt.ca/image-server/v1/mp/designs/1005275915,width=178,height=178/d20.png" alt="die"/></span></button>
+        </div>
+      );
+    }
+  };
+
+  rollTheDieCon = () => {
+    const tempCharacter = {...this.state.newCharacter};
+    tempCharacter.constitution = dieroll(5, 20);
+    this.setState({newCharacter: tempCharacter});
+    this.setState({conRolled: true});
+  };
+  rollTheDieFort = () => {
+    const tempCharacter = {...this.state.newCharacter};
+    tempCharacter.fortitude = dieroll(5, 20);
+    this.setState({newCharacter: tempCharacter});
+    this.setState({fortRolled: true});
+  };
+  rollTheDieRes = () => {
+    const tempCharacter = {...this.state.newCharacter};
+    tempCharacter.resilience = dieroll(5, 20);
+    this.setState({newCharacter: tempCharacter});
+    this.setState({resRolled: true});
+  };
+  rollTheDieStr = () => {
+    const tempCharacter = {...this.state.newCharacter};
+    tempCharacter.strength = dieroll(5, 20);
+    this.setState({newCharacter: tempCharacter});
+    this.setState({strRolled: true});
+  };
+
+  fortButtons = () => {
+    if (this.state.fortRolled) {
+      return (
+        <div>
+          <div>
+            <h3>Psyche:</h3>
+            <h3>{this.state.newCharacter.totalPsyche}</h3>
+          </div>
+          <div>
+            <h3>Fortitude: {this.state.newCharacter.fortitude}</h3>
+            <button type="button" onClick={this.subtractFortPoint.bind()} className="btn btn-default"><span className="glyphicon glyphicon-minus"></span></button>
+            <button type="button" onClick={this.addFortPoint.bind()} className="btn btn-default"><span className="glyphicon glyphicon-plus"></span></button>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <h3>Roll Fortitude:</h3>
+          <button onClick={this.rollTheDieFort.bind()}><span><img src="https://www.spreadshirt.ca/image-server/v1/mp/designs/1005275915,width=178,height=178/d20.png" alt="die"/></span></button>
+        </div>
+      );
+    }
+  };
+  resButtons = () => {
+    if (this.state.resRolled) {
+      return (
+        <div>
+          <div>
+            <h3>Defense: {this.state.newCharacter.defense}</h3>
+          </div>
+          <div>
+            <h3>Resilience: {this.state.newCharacter.resilience}</h3>
+            <button type="button" onClick={this.subtractResPoint.bind()} className="btn btn-default"><span className="glyphicon glyphicon-minus"></span></button>
+            <button type="button" onClick={this.addResPoint.bind()} className="btn btn-default"><span className="glyphicon glyphicon-plus"></span></button>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <h3>Roll Resilience:</h3>
+          <button onClick={this.rollTheDieRes.bind()}><span><img src="https://www.spreadshirt.ca/image-server/v1/mp/designs/1005275915,width=178,height=178/d20.png" alt="die"/></span></button>
+        </div>
+      );
+    }
+  };
+  strButtons = () => {
+    if (this.state.strRolled) {
+      return (
+        <div>
+          <div>
+            <h3>Attack Modifier:</h3>
+            <h3>{this.state.newCharacter.attack}</h3>
+          </div>
+          <div>
+            <h3>Strength:</h3>
+            <h3>{this.state.newCharacter.strength}</h3>
+            <button type="button" onClick={this.subtractStrPoint.bind()} className="btn btn-default"><span className="glyphicon glyphicon-minus"></span></button>
+            <button type="button" onClick={this.addStrPoint.bind()} className="btn btn-default"><span className="glyphicon glyphicon-plus"></span></button>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <h3>Roll Strength:</h3>
+          <button onClick={this.rollTheDieStr.bind()}><span><img src="https://www.spreadshirt.ca/image-server/v1/mp/designs/1005275915,width=178,height=178/d20.png" alt="die"/></span></button>
+        </div>
+      );
+    }
+  };
+
   render () {
     const {newCharacter} = this.state;
     return (
@@ -205,73 +315,22 @@ class CharacterForm extends React.Component {
           </fieldset>
           <button type="submit">Save Character</button>
         </form>
+
         <div className="row">
           <h3>Points Pot: {this.state.pointsPot}</h3>
         </div>
         <div>
           <div className="row">
-            <fieldset className="col-sm-6">
-              <h3>Health:</h3>
-              <h3>
-                {this.state.newCharacter.totalHealth}
-              </h3>
-            </fieldset>
-            <fieldset className="col-sm-6">
-              <h3>Constitution:</h3>
-              <h3>
-                {this.state.newCharacter.constitution}
-              </h3>
-              <button type="button" onClick={this.subtractConPoint.bind()} className="btn btn-default"><span className="glyphicon glyphicon-minus"></span></button>
-              <button type="button" onClick={this.addConPoint.bind()} className="btn btn-default"><span className="glyphicon glyphicon-plus"></span></button>
-            </fieldset>
+            {this.conButtons()}
           </div>
           <div className="row">
-            <fieldset className="col-sm-6">
-              <h3>Psyche:</h3>
-              <h3>
-                {this.state.newCharacter.totalPsyche}
-              </h3>
-            </fieldset>
-            <fieldset className="col-sm-6">
-              <h3>Fortitude:</h3>
-              <h3>
-                {this.state.newCharacter.fortitude}
-              </h3>
-              <button type="button" onClick={this.subtractFortPoint.bind()} className="btn btn-default"><span className="glyphicon glyphicon-minus"></span></button>
-              <button type="button" onClick={this.addFortPoint.bind()} className="btn btn-default"><span className="glyphicon glyphicon-plus"></span></button>
-            </fieldset>
+            {this.fortButtons()}
           </div>
           <div className="row">
-            <fieldset className="col-sm-6">
-              <h3>Attack Modifier:</h3>
-              <h3>
-                {this.state.newCharacter.attackModifier}
-              </h3>
-            </fieldset>
-            <fieldset className="col-sm-6">
-              <h3>Strength:</h3>
-              <h3>
-                {this.state.newCharacter.strength}
-              </h3>
-              <button type="button" onClick={this.subtractStrPoint.bind()} className="btn btn-default"><span className="glyphicon glyphicon-minus"></span></button>
-              <button type="button" onClick={this.addStrPoint.bind()} className="btn btn-default"><span className="glyphicon glyphicon-plus"></span></button>
-            </fieldset>
+            {this.strButtons()}
           </div>
           <div className="row">
-            <fieldset className="col-sm-6">
-              <h3>Defense:</h3>
-              <h3>
-                {this.state.newCharacter.defense}
-              </h3>
-            </fieldset>
-            <fieldset className="col-sm-6">
-              <h3>Resilience:</h3>
-              <h3>
-                {this.state.newCharacter.resilience}
-              </h3>
-              <button type="button" onClick={this.subtractResPoint.bind()} className="btn btn-default"><span className="glyphicon glyphicon-minus"></span></button>
-              <button type="button" onClick={this.addResPoint.bind()} className="btn btn-default"><span className="glyphicon glyphicon-plus"></span></button>
-            </fieldset>
+            {this.resButtons()}
           </div>
         </div>
       </div>
