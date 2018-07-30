6167f9d2b7b804ef63e69b936626f18bb3684d6d
Amanda Slayton <alslayton86@gmail.com>
2018-07-30 01:06:09 -0500

    wip

--- src/components/CharacterForm/CharacterForm.js
+++ src/components/CharacterForm/CharacterForm.js
@@ -29,6 +29,122 @@ class CharacterForm extends React.Component {
   }
   state = {
     newCharacter: defaultCharacter,
+    pointsPot: 12,
+    conCount: 0,
+    fortCount: 0,
+    resCount: 0,
+    strCount: 0,
+  };
+
+  evalStats = (val) => {
+    if (val < 4) {
+      return (-2);
+    } else if (val >= 4 && val < 10) {
+      return (-1);
+    } else if (val >= 10 && val < 13) {
+      return (0);
+    } else if (val >= 13 && val < 18) {
+      return (1);
+    } else {
+      return (2);
+    }
+  };
+
+  addConPoint = () => {
+    if (this.state.pointsPot > 0 && this.state.conCount < 3) {
+      const tempCharacter = {...this.state.newCharacter};
+      tempCharacter.constitution = (this.state.newCharacter.constitution * 1) + 1;
+      this.setState({newCharacter: tempCharacter});
+      const tempPointsPot = (this.state.pointsPot * 1) - 1;
+      this.setState({pointsPot: tempPointsPot});
+      const tempConCount = (this.state.conCount * 1) + 1;
+      this.setState({conCount: tempConCount});
+    }
+    const modifier = this.evalStats(this.state.newCharacter.constitution);
+    const modCharacter = {...this.state.newCharacter};
+    modCharacter.totalHeath = (this.state.newCharacter.totalHealth * 1) + modifier;
+    this.setState({newCharacter: modCharacter});
+  };
+  subtractConPoint = () => {
+    if (this.state.conCount > -3) {
+      const tempCharacter = {...this.state.newCharacter};
+      tempCharacter.constitution = (this.state.newCharacter.constitution * 1) - 1;
+      this.setState({newCharacter: tempCharacter});
+      const tempPointsPot = (this.state.pointsPot * 1) + 1;
+      this.setState({pointsPot: tempPointsPot});
+      const tempConCount = (this.state.conCount * 1) - 1;
+      this.setState({conCount: tempConCount});
+    }
+  };
+
+  addFortPoint = () => {
+    console.log(this.state.fortCount);
+    if (this.state.pointsPot > 0 && this.state.fortCount < 3) {
+      const tempCharacter = {...this.state.newCharacter};
+      tempCharacter.fortitude = (this.state.newCharacter.fortitude * 1) + 1;
+      this.setState({newCharacter: tempCharacter});
+      const tempPointsPot = (this.state.pointsPot * 1) - 1;
+      this.setState({pointsPot: tempPointsPot});
+      const tempFortCount = (this.state.fortCount * 1) + 1;
+      this.setState({fortCount: tempFortCount});
+    }
+  };
+  subtractFortPoint = () => {
+    if (this.state.fortCount > -3) {
+      const tempCharacter = {...this.state.newCharacter};
+      tempCharacter.fortitude = (this.state.newCharacter.fortitude * 1) - 1;
+      this.setState({newCharacter: tempCharacter});
+      const tempPointsPot = (this.state.pointsPot * 1) + 1;
+      this.setState({pointsPot: tempPointsPot});
+      const tempFortCount = (this.state.fortCount * 1) - 1;
+      this.setState({fortCount: tempFortCount});
+    }
+  };
+
+  addStrPoint = () => {
+    if (this.state.pointsPot > 0 && this.state.strCount < 3) {
+      const tempCharacter = {...this.state.newCharacter};
+      tempCharacter.strength = (this.state.newCharacter.strength * 1) + 1;
+      this.setState({newCharacter: tempCharacter});
+      const tempPointsPot = (this.state.pointsPot * 1) - 1;
+      this.setState({pointsPot: tempPointsPot});
+      const tempStrCount = (this.state.strCount * 1) + 1;
+      this.setState({strCount: tempStrCount});
+    }
+  };
+  subtractStrPoint = () => {
+    if (this.state.conCount > -3) {
+      const tempCharacter = {...this.state.newCharacter};
+      tempCharacter.strength = (this.state.newCharacter.strength * 1) - 1;
+      this.setState({newCharacter: tempCharacter});
+      const tempPointsPot = (this.state.pointsPot * 1) + 1;
+      this.setState({pointsPot: tempPointsPot});
+      const tempStrCount = (this.state.strCount * 1) - 1;
+      this.setState({strCount: tempStrCount});
+    }
+  };
+
+  addResPoint = () => {
+    if (this.state.pointsPot > 0 && this.state.resCount < 3) {
+      const tempCharacter = {...this.state.newCharacter};
+      tempCharacter.resilience = (this.state.newCharacter.resilience * 1) + 1;
+      this.setState({newCharacter: tempCharacter});
+      const tempPointsPot = (this.state.pointsPot * 1) - 1;
+      this.setState({pointsPot: tempPointsPot});
+      const tempResCount = (this.state.resCount * 1) + 1;
+      this.setState({resCount: tempResCount});
+    }
+  };
+  subtractResPoint = () => {
+    if (this.state.resCount > -3) {
+      const tempCharacter = {...this.state.newCharacter};
+      tempCharacter.resilience = (this.state.newCharacter.resilience * 1) - 1;
+      this.setState({newCharacter: tempCharacter});
+      const tempPointsPot = (this.state.pointsPot * 1) + 1;
+      this.setState({pointsPot: tempPointsPot});
+      const tempResCount = (this.state.resCount * 1) - 1;
+      this.setState({resCount: tempResCount});
+    }
   };
 
   formFieldStringState = (name, e) => {
@@ -87,118 +203,77 @@ class CharacterForm extends React.Component {
               onChange={this.imageChange}
             />
           </fieldset>
-          <fieldset>
-            <label htmlFor="currentHealth">Health:</label>
-            <br/>
-            <h3
-              type="text"
-              id="totalHealth"
-              placeholder="Health"
-            >
-              {defaultCharacter.totalHealth}
-            </h3>
-          </fieldset>
-          <fieldset>
-            <label htmlFor="totalPsyche">Psyche:</label>
-            <br/>
-            <h3
-              type="text"
-              id="totalPsyche"
-              placeholder="Psyche"
-            >
-              {defaultCharacter.totalPsyche}
-            </h3>
-          </fieldset>
-          <fieldset>
-            <label htmlFor="attackModifier">Attack Modifier:</label>
-            <br/>
-            <h3
-              type="text"
-              id="attackModifier"
-              placeholder="Modifier"
-            >
-              {defaultCharacter.attackModifier}
-            </h3>
-          </fieldset>
-          <fieldset>
-            <label htmlFor="defense">Defense:</label>
-            <br/>
-            <h3
-              type="text"
-              id="defense"
-              placeholder=" URL"
-            >
-              {defaultCharacter.defense}
-            </h3>
-          </fieldset>
-          <fieldset>
-            <label htmlFor="strength">Strength:</label>
-            <br/>
-            <h3
-              type="text"
-              id="strength"
-              placeholder="Strength"
-            >
-              {defaultCharacter.strength}
-            </h3>
-          </fieldset>
-          <fieldset>
-            <label htmlFor="fortitude">Fortitude:</label>
-            <br/>
-            <h3
-              type="text"
-              id="fortitude"
-              placeholder="Fortitude"
-            >
-              {defaultCharacter.fortitude}
-            </h3>
-          </fieldset>
-          <fieldset>
-            <label htmlFor="resilience">Resilience:</label>
-            <br/>
-            <h3
-              type="text"
-              id="resilience"
-              placeholder="resilience"
-            >
-              {defaultCharacter.resilience}
-            </h3>
-          </fieldset>
-          <fieldset>
-            <label htmlFor="constitution">Constitution:</label>
-            <br/>
-            <h3
-              type="text"
-              id="constitution"
-              placeholder="Constitution"
-            >
-              {defaultCharacter.constitution}
-            </h3>
-          </fieldset>
-          <fieldset>
-            <label htmlFor="exp">Experience:</label>
-            <br/>
-            <h3
-              type="text"
-              id="exp"
-              placeholder="experience"
-            >
-              {defaultCharacter.exp}
-            </h3>
-          </fieldset>
-          <fieldset>
-            <label htmlFor="level">Level:</label>
-            <br/>
-            <h3
-              type="text"
-              id="level"
-              placeholder="Level"
-            >
-              {newCharacter.level}
-            </h3>
-          </fieldset>
-          <button>Save Character</button>
+          <button type="submit">Save Character</button>
         </form>
+        <div className="row">
+          <h3>Points Pot: {this.state.pointsPot}</h3>
+        </div>
+        <div>
+          <div className="row">
+            <fieldset className="col-sm-6">
+              <h3>Health:</h3>
+              <h3>
+                {this.state.newCharacter.totalHealth}
+              </h3>
+            </fieldset>
+            <fieldset className="col-sm-6">
+              <h3>Constitution:</h3>
+              <h3>
+                {this.state.newCharacter.constitution}
+              </h3>
+              <button type="button" onClick={this.subtractConPoint.bind()} className="btn btn-default"><span className="glyphicon glyphicon-minus"></span></button>
+              <button type="button" onClick={this.addConPoint.bind()} className="btn btn-default"><span className="glyphicon glyphicon-plus"></span></button>
+            </fieldset>
+          </div>
+          <div className="row">
+            <fieldset className="col-sm-6">
+              <h3>Psyche:</h3>
+              <h3>
+                {this.state.newCharacter.totalPsyche}
+              </h3>
+            </fieldset>
+            <fieldset className="col-sm-6">
+              <h3>Fortitude:</h3>
+              <h3>
+                {this.state.newCharacter.fortitude}
+              </h3>
+              <button type="button" onClick={this.subtractFortPoint.bind()} className="btn btn-default"><span className="glyphicon glyphicon-minus"></span></button>
+              <button type="button" onClick={this.addFortPoint.bind()} className="btn btn-default"><span className="glyphicon glyphicon-plus"></span></button>
+            </fieldset>
+          </div>
+          <div className="row">
+            <fieldset className="col-sm-6">
+              <h3>Attack Modifier:</h3>
+              <h3>
+                {this.state.newCharacter.attackModifier}
+              </h3>
+            </fieldset>
+            <fieldset className="col-sm-6">
+              <h3>Strength:</h3>
+              <h3>
+                {this.state.newCharacter.strength}
+              </h3>
+              <button type="button" onClick={this.subtractStrPoint.bind()} className="btn btn-default"><span className="glyphicon glyphicon-minus"></span></button>
+              <button type="button" onClick={this.addStrPoint.bind()} className="btn btn-default"><span className="glyphicon glyphicon-plus"></span></button>
+            </fieldset>
+          </div>
+          <div className="row">
+            <fieldset className="col-sm-6">
+              <h3>Defense:</h3>
+              <h3>
+                {this.state.newCharacter.defense}
+              </h3>
+            </fieldset>
+            <fieldset className="col-sm-6">
+              <h3>Resilience:</h3>
+              <h3>
+                {this.state.newCharacter.resilience}
+              </h3>
+              <button type="button" onClick={this.subtractResPoint.bind()} className="btn btn-default"><span className="glyphicon glyphicon-minus"></span></button>
+              <button type="button" onClick={this.addResPoint.bind()} className="btn btn-default"><span className="glyphicon glyphicon-plus"></span></button>
+            </fieldset>
+          </div>
+        </div>
       </div>
     );
   };
