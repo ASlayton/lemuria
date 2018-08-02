024c12cb8984bf0be7dcb2695060cb72dc620ed5
Amanda Slayton <alslayton86@gmail.com>
2018-08-01 21:16:50 -0500

    psyche monsters do psyche damage now

--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -158,36 +158,71 @@ class Events extends React.Component {
     const attackRoll = dieroll(1, 20);
     const getRandom = dieroll(0,9) * 1;
     const playerDefense = this.props.player.defense * 1;
-    if (attackRoll === 1) {
-      const enemyDmg = dieroll(1, 6);
-      const enemy = Object.assign({}, this.state.enemy);
-      enemy.currentHealth = enemy.currentHealth - enemyDmg;
-      this.setState({enemy});
-      this.setState({eDmgResult: enemyDmg});
-      const eGameMsg = this.state.combatMsg[1].msg;
-      this.setState({eGameMsg});
-    } else if (attackRoll === 20) {
-      const playerDmg = dieroll(1, 12);
-      const player = Object.assign({}, this.props.player);
-      player.currentHealth = player.currentHealth - playerDmg;
-      this.props.playerHandler(player);
-      this.setState({eDmgResult: playerDmg});
-      const eGameMsg = this.state.combatMsg[0].msg;
-      this.setState({eGameMsg});
-    } else if (attackRoll >= playerDefense) {
-      const playerDmg = dieroll(1, 6);
-      const player = Object.assign({}, this.props.player);
-      player.currentHealth = player.currentHealth - playerDmg;
-      this.props.playerHandler(player);
-      this.setState({eDmgResult: playerDmg});
-      const eGameMsg = this.state.combatMsg[4].msg[getRandom];
-      this.setState({eGameMsg});
-    } else if (attackRoll < playerDefense) {
-      this.setState({eDmgResult: 0});
-      const eGameMsg = this.state.combatMsg[5].msg[getRandom];
-      this.setState({eGameMsg});
-    } else {
-      console.error('Something is not being evaluated correctly.');
+    if (this.state.enemy.damageType === 'health') {
+      if (attackRoll === 1) {
+        const enemyDmg = dieroll(1, 6);
+        const enemy = Object.assign({}, this.state.enemy);
+        enemy.currentHealth = enemy.currentHealth - enemyDmg;
+        this.setState({enemy});
+        this.setState({eDmgResult: enemyDmg});
+        const eGameMsg = this.state.combatMsg[1].msg;
+        this.setState({eGameMsg});
+      } else if (attackRoll === 20) {
+        const playerDmg = dieroll(1, 12);
+        const player = Object.assign({}, this.props.player);
+        player.currentHealth = player.currentHealth - playerDmg;
+        this.props.playerHandler(player);
+        this.setState({eDmgResult: playerDmg});
+        const eGameMsg = this.state.combatMsg[0].msg;
+        this.setState({eGameMsg});
+      } else if (attackRoll >= playerDefense) {
+        const playerDmg = dieroll(1, 6);
+        const player = Object.assign({}, this.props.player);
+        player.currentHealth = player.currentHealth - playerDmg;
+        this.props.playerHandler(player);
+        this.setState({eDmgResult: playerDmg});
+        const eGameMsg = this.state.combatMsg[4].msg[getRandom];
+        this.setState({eGameMsg});
+      } else if (attackRoll < playerDefense) {
+        this.setState({eDmgResult: 0});
+        const eGameMsg = this.state.combatMsg[5].msg[getRandom];
+        this.setState({eGameMsg});
+      } else {
+        console.error('Something is not being evaluated correctly.');
+      };
+    } else if (this.state.enemy.damageType === 'psyche') {
+      if (attackRoll === 1) {
+        const enemyDmg = dieroll(1, 6);
+        const enemy = Object.assign({}, this.state.enemy);
+        enemy.currentHealth = enemy.currentHealth - enemyDmg;
+        this.setState({enemy});
+        this.setState({eDmgResult: enemyDmg});
+        const eGameMsg = this.state.combatMsg[1].msg;
+        this.setState({eGameMsg});
+      } else if (attackRoll === 20) {
+        const playerDmg = dieroll(1, 12);
+        const player = Object.assign({}, this.props.player);
+        player.currentPsyche = player.currentPsyche - playerDmg;
+        this.props.playerHandler(player);
+        this.setState({eDmgResult: playerDmg});
+        const eGameMsg = this.state.combatMsg[0].msg;
+        this.setState({eGameMsg});
+      } else if (attackRoll >= playerDefense) {
+        const playerDmg = dieroll(1, 6);
+        const player = Object.assign({}, this.props.player);
+        player.currentPsyche = player.currentPsyche - playerDmg;
+        this.props.playerHandler(player);
+        this.setState({eDmgResult: playerDmg});
+        const eGameMsg = this.state.combatMsg[4].msg[getRandom];
+        this.setState({eGameMsg});
+      } else if (attackRoll < playerDefense) {
+        this.setState({eDmgResult: 0});
+        const eGameMsg = this.state.combatMsg[5].msg[getRandom];
+        this.setState({eGameMsg});
+      } else {
+        console.error('Something is not being evaluated correctly.');
+      };
+
     };
     this.evaluateStatus('enemyTurn');
   };
